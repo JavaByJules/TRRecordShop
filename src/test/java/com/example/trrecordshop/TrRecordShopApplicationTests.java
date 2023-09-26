a492e548593a24e73dd8e4075f505ecd00aead0e
chadrakh <chadrak@outlook.com>
2023-09-26 15:07:53 +0100

    Initial commit

---
+++ src/test/java/com/example/trrecordshop/TrRecordShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trrecordshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrRecordShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
