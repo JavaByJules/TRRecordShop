675362083f470e8a5e19fd01c872f1bf2e577b9a
chadrakh <chadrak@outlook.com>
2023-09-26 16:04:11 +0100

    Feature: Add Album Repository

--- src/main/java/com/example/trrecordshop/repository/AlbumRepository.java
+++ src/main/java/com/example/trrecordshop/repository/AlbumRepository.java
@@ -1,4 +1,10 @@
 package com.example.trrecordshop.repository;
 
-public class AlbumRepository {
+import com.example.trrecordshop.model.Album;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AlbumRepository extends CrudRepository<Album, Long> {
+
 }
