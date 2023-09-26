74b469ae21820fe1ed78d9fc67c0855ad327369a
chadrakh <chadrak@outlook.com>
2023-09-26 15:51:34 +0100

    Feature: Add Genre Enum

--- src/main/java/com/example/trrecordshop/model/Genre.java
+++ src/main/java/com/example/trrecordshop/model/Genre.java
@@ -1,4 +1,15 @@
 package com.example.trrecordshop.model;
 
 public enum Genre {
+    DANCE,
+    POP,
+    ROCK,
+    METAL,
+    ELECTRONIC,
+    AFROBEATS,
+    BLUES,
+    HIPHOP,
+    RNB,
+    CLASSICAL,
+    OTHER
 }
