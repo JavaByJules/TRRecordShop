7987bbfb8278b1cec1c804c7bb04d047787aa276
Adrian Yates <adeyatz@gmail.com>
2023-09-27 17:01:17 +0100

    RecordShopService: Amended methods GetAlbumsByReleaseYear(int) and updateAlbumByID() to return boolean

--- src/main/java/com/example/trrecordshop/service/RecordShopService.java
+++ src/main/java/com/example/trrecordshop/service/RecordShopService.java
@@ -1,5 +1,28 @@
 package com.example.trrecordshop.service;
 
-public class RecordShopService {
 
-}
+import com.example.trrecordshop.model.Album;
+import com.example.trrecordshop.model.Genre;
+
+import java.util.List;
+
+    public interface RecordShopService {
+        List<Album> getAllAlbums();
+
+        List<Album> getAlbumsByArtist (String artist);
+
+        List <Album> getAlbumsByReleaseYear (int releaseYear);
+
+        List <Album> getAlbumsByGenre (Genre genre);
+
+        Album getAlbumByAlbumName (String albumName);
+
+        Album insertAlbum (Album album);
+
+        boolean updateAlbumById (Long id, Album album);
+
+        int updateStock (Long id, int amount, boolean incrementing);
+
+        boolean deleteByAlbumId (Long id);
+
+    }
