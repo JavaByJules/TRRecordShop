59f11fb319872460e120917eb6df1c3606c35dc3
chadrakh <chadrak@outlook.com>
2023-09-28 02:30:51 +0100

    Feature: Add Controller Mapping Logic

--- src/main/java/com/example/trrecordshop/controller/RecordShopController.java
+++ src/main/java/com/example/trrecordshop/controller/RecordShopController.java
@@ -1,35 +1,94 @@
 package com.example.trrecordshop.controller;
 
 import com.example.trrecordshop.model.Album;
+import com.example.trrecordshop.model.Genre;
+import com.example.trrecordshop.service.RecordShopService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/v1")
 public class RecordShopController {
+
+    private final RecordShopService recordShopService;
+
+    @Autowired
+    public RecordShopController(RecordShopService recordShopService) {
+        this.recordShopService = recordShopService;
+    }
+
     @GetMapping("/")
     public String welcome() {
-        return "Welcome Page";
+        return "Welcome to the TR Record Shop Backend";
     }
 
     @GetMapping("/albums")
-    public List<Album> getAllAlbums() {
-        return null;
+    public ResponseEntity<List<Album>> getAllAlbums() {
+        List<Album> albums = recordShopService.getAllAlbums();
+        return new ResponseEntity<>(albums, HttpStatus.OK);
     }
 
     @GetMapping("/album")
     @ResponseBody
-    public List<Album> getAlbumByCriteria(
+    public ResponseEntity<List<Album>> getAlbumByCriteria(
             @RequestParam(name = "title", required = false) String title,
             @RequestParam(name = "artist", required = false) String artist,
             @RequestParam(name = "releaseYear", required = false) String releaseYear,
             @RequestParam(name = "genre", required = false) String genre) {
 
-        // if param is not null
-        // implement logic to return response
+        List<Album> albums = new ArrayList<>();
 
-        return null;
+        if (title != null) {
+            albums = recordShopService.getAlbumsByTitle(title);
+        }
+
+        if (artist != null) {
+            albums = recordShopService.getAlbumsByArtist(artist);
+        }
+
+        if (releaseYear != null) {
+            int releaseYearParsed = Integer.parseInt(releaseYear);
+            albums = recordShopService.getAlbumsByReleaseYear(releaseYearParsed);
+        }
+
+        if (genre != null) {
+            albums = recordShopService.getAlbumsByGenre(Genre.valueOf(genre));
+        }
+
+        return new ResponseEntity<>(albums, HttpStatus.OK);
     }
+
+    @PostMapping
+    public ResponseEntity<Album> addAlbum(@RequestBody Album album) {
+        Album newAlbum = recordShopService.insertAlbum(album);
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add("album", "/api/v1/album/" + newAlbum.getId().toString());
+
+        return new ResponseEntity<>(newAlbum, httpHeaders, HttpStatus.CREATED);
+    }
+
+    //@PatchMapping
+    //public ResponseEntity<Album> updateAlbum(
+    //        @RequestParam(name = "title") String title,
+    //        @RequestParam(name = "artist") String artist,
+    //        @RequestBody Album updatedAlbum) {
+
+    //    Optional<Album> targetAlbum = recordShopService.getAllAlbums().stream()
+    //            .filter(album -> album.getTitle().equals(title))
+    //            .filter(album -> album.getArtist().equals(artist))
+    //            .findFirst();
+
+    //    recordShopService.updateAlbumById(targetAlbum.getId(), updatedAlbum);
+
+    //    return new ResponseEntity<>(recordShopService.getAlbumById, HttpStatus.OK);
+    //}
 
 }
